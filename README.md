3381a224175d872eb939a280692723ae593e42ae
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-09-24 21:00:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,69 @@
+# Discord-Rich-Presence
+
+Discord Rich Presence is a Rich Presence (RPC) client that show the channel, user or group you're talking to.
+
+Original project made by [Xanll](https://github.com/Xanll)
+
+## Getting Started
+
+1. Fork or download this project
+2. Go to the [Applications](https://discordapp.com/developers/applications/) page and create a new application. 
+   1. The name should be whatever you want to be for the status, I.E. "Playing with my friends"
+3. Now go to the `Rich Presence` tab > `Art Assets`
+   1. Add images you would like to be displayed on the RPC. Make sure to give them names
+4. Rename `example-config.json` to `config.json`
+5. Edit the properties you need (See Configuration)
+
+## Running
+
+```
+node index.js or npm start
+```
+
+You can also double click on `start.bat` to run this project in Windows CMD
+
+## Configuration
+
+`*` = anywhere in the config
+
+- `appid` The application ID. `string`
+- `interval` Delay in seconds it will update. If you're experiencing lag, you might want to bump this up. `number`
+- `...discord.details` What it will display in the RP when chatting in a group, channel or to a user. `array.string`
+- `...sleep.details` Some text it will display in the RP when sleeping. `string`
+- `...sleep.state` Additional text when sleeping. `string`
+- `*.largeImageKey` Name of the large image you want. `string`
+- `*.largeImageText` Text you want to show when hovering over the large image `string`
+- `*.smallImageKey` Name of the small image you want. `string`
+- `*.smallImageText` Text you want to show when hovering over the small image. `string`
+- `*.startTimestamp` Display time since the RPC updated. `true/false`
+- `*.endTimestamp` Display time until the current task (ex. sleeping) is done. `true/false`
+- `*.joinButton` Display a "Ask to Join"-button that's greyed out (since it's not a game). `true/false`
+- `*.spectateButton` Same as above, just as a "Spectate"-button. `true/false`
+- `sleepTime` When you go to sleep. (See #Sleep Times)
+
+## Sleep Times
+
+Each array has his own day, first is Monday and last is Sunday
+
+```
+"sleepTime": [
+        [ 22, 7  ], <= Monday
+        [ 22, 7  ],
+        [ 22, 7  ],
+        [ 22, 7  ],
+        [ 22, 7  ],
+        [ 23, 10 ],
+        [ 23, 10 ]  <= Sunday
+    ]
+```
+
+| Days | Sleep time | Wake up time | 
+| :----: | :----: | :----: |
+| Monday | 22 PM | 7 AM |
+| Tuesday | 22 PM | 7 AM |
+| Wednesday | 22 PM | 7 AM |
+| Thursday | 22 PM | 7 AM |
+| Friday | 22 PM | 7 AM |
+| Saturday | 23 PM | 10 AM |
+| Sunday | 23 PM | 10 AM |
+
---
+++ index.js
@@ -0,0 +1,108 @@
+import DiscordRPC from 'discord-rpc';
+import fs from 'fs';
+// Config file
+const config = Object.assign(JSON.parse(fs.readFileSync('./example-config.json', 'utf8')), JSON.parse(fs.readFileSync('./config.json', 'utf8')));
+// App ID & RPC Client.
+const AppID = config.appid;
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+
+let currentArray,
+    channel,
+    details;
+
+// Update Time.
+function update() {
+
+    if (currentlyAsleep()) return setActivity({
+        details: config.rpc.sleep.details || undefined,
+        state: config.rpc.sleep.state || undefined,
+        largeImageKey: config.rpc.sleep.largeImageKey || undefined,
+        largeImageText: config.rpc.sleep.largeImageText || undefined,
+        endTimestamp: config.rpc.sleep.endTimestamp ? awakeWhen() : undefined,
+		joinSecret: config.rpc.sleep.joinButton ? true : false,
+		spectateSecret: config.rpc.sleep.spectateButton ? true : false
+    })
+
+    setActivity({
+        details: details,
+        state: channel,
+        startTimestamp: config.rpc.discord.startTimestamp || undefined,
+        largeImageKey: config.rpc.discord.largeImageKey || undefined,
+        largeImageText: config.rpc.discord.largeImageText || undefined,
+        smallImageKey: config.rpc.discord.smallImageKey || undefined,
+        smallImageText: config.rpc.discord.smallImageText || undefined,
+        joinSecret: config.rpc.discord.joinButton || undefined,
+        spectateSecret: config.rpc.discord.spectateButton || undefined
+    })
+}
+
+// When Ready.
+rpc.on('ready', async () => {
+    console.log("Ready! The RPC Client Started Succesfully.")
+
+    setInterval(() => {
+		try { update(); } catch (e) {}
+    }, config.interval * 1000);
+
+});
+
+// Login.
+rpc.login({ clientId: AppID }).catch(console.error);
+
+// Adding Activity
+function setActivity(array) {
+    if (JSON.stringify(array, null, 4) == JSON.stringify(currentArray, null, 4)) return; // array == currentArray, prevents API spamming
+    currentArray = array;
+
+    let time = new Date();
+
+    array = {
+        details: array.details,
+        state: array.state,
+        startTimestamp: array.startTimestamp ? time : undefined,
+        endTimestamp: array.endTimestamp,
+        largeImageKey: array.largeImageKey,
+        largeImageText: array.largeImageText,
+        smallImageKey: array.smallImageKey,
+        smallImageText: array.smallImageText,
+        partyId: AppID,
+        joinSecret: array.joinSecret ? AppID + "JOIN" : undefined,
+		spectateSecret: array.spectateSecret ? AppID + "SPEC" : undefined,
+        instance: false
+    }
+
+    rpc.setActivity(array)
+}
+
+// Sleep Time (Fetch)
+function getSleepTimes() {
+    let day = (new Date().getHours() > 12 ? ((new Date().getDay() + 1) == 8 ? 1 : new Date().getDay() + 1) : new Date().getDay()) - 1;
+
+    if (config.sleepTime) return config.sleepTime[day]
+    else return null;
+}
+
+// Sleep Time Update.
+function currentlyAsleep() {
+    let sleepTimes = getSleepTimes();
+
+    if (!sleepTimes) return false; // the user don't want the sleep module
+
+    if (new Date().getHours() < sleepTimes[1] || new Date().getHours() >= sleepTimes[0]) {
+        return true;
+    }
+    return false;
+}
+
+// When Awake.
+function awakeWhen() {
+    let time = new Date();
+    let awakeTime = new Date();
+
+    if (time.getHours() > 12) {
+        awakeTime.setDate(awakeTime.getDate() + 1)
+    }
+
+    awakeTime.setHours(getSleepTimes()[1], 0, 0, 0);
+    return awakeTime;
+}
