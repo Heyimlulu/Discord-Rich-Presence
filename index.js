f097fb848ec2cae9d6fbc6f9d2c22fc79fcc74ff
Lucas <lucas.rouillermonay@gmail.com>
2023-05-18 17:15:42 +0200

    Remove sleep mode

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ inquirer.prompt([
     {
         type: 'list',
         name: 'config',
-        message: 'Select your config file',
+        message: 'Select your configuration file',
         choices: choices,
     },
 ]).then(answers => {
@@ -27,7 +27,6 @@ inquirer.prompt([
     const configFile = path.join(configDir, selectedConfig);
     config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
 
-    console.log("\x1b[32mReady! \x1b[34mThe RPC Client Started Successfully.\x1b[0m");
     client.login({
         clientId: config.appid
     }).catch(console.error);
@@ -38,24 +37,6 @@ client.on('ready', async () => {
     setActivity(config.rpc.discord);
     console.log("\x1b[32mReady! \x1b[34mThe RPC Client Started Successfully.\x1b[0m");
 });
-
-// Update activity status
-function updateActivity() {
-    /*
-    if (isCurrentlyAsleep()) {
-        setActivity(config.rpc.sleep);
-    } else {
-        setActivity(config.rpc.discord);
-    }
-
-     */
-
-    setActivity(config.rpc.discord);
-
-    const interval = config.interval || 5000; // Default interval of 5 seconds
-
-    setTimeout(updateActivity, interval);
-}
 
 // Set activity => Set current activity for the user
 function setActivity(activityConfig) {
@@ -90,50 +71,3 @@ function setActivity(activityConfig) {
 
     client.setActivity(activity);
 }
-
-// Sleep Time => Return the current day [Number]
-/*
-function getSleepTimes() {
-    let day = 0;
-    const currentHour = new Date().getHours();
-
-    if (currentHour > 12) {
-        day = (new Date().getDay() + 1) % 7;
-    } else {
-        day = new Date().getDay();
-    }
-
-    day--;
-
-    return config.sleepTime ? config.sleepTime[day] : null;
-}
-*/
-
-// Sleep Time Update => Return true or false [Boolean]
-/*
-function isCurrentlyAsleep() {
-    const sleepTimes = getSleepTimes();
-
-    if (!sleepTimes) {
-        return false;
-    }
-
-    const currentHour = new Date().getHours();
-    return currentHour < sleepTimes[1] || currentHour >= sleepTimes[0];
-}
-*/
-
-// When Awake => Return when the user is waking up [Date]
-/*
-function awakeWhen() {
-    const time = new Date();
-    const awakeTime = new Date();
-
-    if (time.getHours() > 12) {
-        awakeTime.setDate(awakeTime.getDate() + 1);
-    }
-
-    awakeTime.setHours(getSleepTimes()[1], 0, 0, 0);
-    return awakeTime;
-}
-*/
